419a0336bd7ea7c4425273619748338e442ed023
User4chan <14PasMyk@tdms.worcs.sch.uk>
2024-10-14 18:24:16 +0100

    first commit

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const btnModal = document.querySelector('.main__button')
 const modalOverlay = document.querySelector('.modal-overlay')
 const btnCloseModal = document.querySelector('.icon-close-box')
 const burgerMenu = document.querySelector('.burger-menu')
-const btnCloseBurgerMenu = document.querySelector('.icon-close-burger-menu')
+const btnCloseBurgerMenu = document.querySelector('.icon-close-box')
 const btnOpenBurgerMenu = document.querySelector('.solid_menu')
+console.log(btnCloseBurgerMenu)
 
-btnModal.addEventListener('click', (event) => {
-    modalOverlay.classList.add('is-open')
-})
-btnCloseModal.addEventListener('click', event => {
-    modalOverlay.classList.remove('is-open')
-})
+// btnModal.addEventListener('click', (event) => {
+//     modalOverlay.classList.add('is-open')
+// })
+// btnCloseModal.addEventListener('click', event => {
+//     modalOverlay.classList.remove('is-open')
+// })
 btnOpenBurgerMenu.addEventListener('click', event => {
     burgerMenu.classList.add('is-open')
 })
